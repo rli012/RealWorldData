35dbd9e5ec6cdb177b466428ac27ca6ff5a3a501
Ruidong Li <rli012@ucr.edu>
2020-03-27 11:49:03 -0700

    Update Drugs.com.R

--- Drugs.com.R
+++ Drugs.com.R
@@ -45,3 +45,60 @@ for (drug in drug1$drugs.D30.all) {
 
 
 ### Drugs.com based
+drugs.az <- c()
+for (c1 in LETTERS) {
+  for (c2 in c(LETTERS, '0-9')) {
+
+    az <- tolower(paste0(c1, c2))
+    fl <- paste0('data/IQVIA/Dispensing/Drugs.com/AZlist/', az, '.html')
+    
+    doc.html = htmlTreeParse(fl, useInternal = TRUE)
+    
+    plain.text <- xpathSApply(doc.html, "//text()[not(ancestor::script)][not(ancestor::style)][not(ancestor::noscript)][not(ancestor::form)]", xmlValue)
+    
+    if (c2=='0-9') {
+      idx <- grep(paste0('^',c1, '.*\\d+'), plain.text, ignore.case = TRUE)
+    } else {
+      idx <- grep(paste0('^',az), plain.text, ignore.case = TRUE)
+    }
+
+    plain.text <- plain.text[idx]
+    filter <- which(duplicated(plain.text))
+    
+    plain.text <- plain.text[-filter]
+    drugs.az <- c(drugs.az, plain.text)
+    
+    
+  }
+}
+
+fl <- paste0('data/IQVIA/Dispensing/Drugs.com/AZlist/0-9.html')
+
+doc.html = htmlTreeParse(fl, useInternal = TRUE)
+
+plain.text <- xpathSApply(doc.html, "//text()[not(ancestor::script)][not(ancestor::style)][not(ancestor::noscript)][not(ancestor::form)]", xmlValue)
+View(plain.text)
+
+idx <- grep('^\\d+', plain.text)
+idx
+
+plain.text <- plain.text[idx]
+plain.text
+
+filter <- which(duplicated(plain.text))
+
+plain.text <- plain.text[-filter]
+drugs.az <- c(drugs.az, plain.text)
+
+site <- gsub("\\(|\\)|\\'", '', drugs.az)
+
+site <- gsub('\\W+\\s*', '-', site)
+site
+
+site <- tolower(site)
+site
+
+drug.info <- data.frame(drugs.az=drugs.az, site=site, stringsAsFactors = F)
+
+write.table(drug.info, file='data/IQVIA/Dispensing/Drugs.com/drugs.az.txt',
+            quote = F, sep = '\t', row.names = F)
